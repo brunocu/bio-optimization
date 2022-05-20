1c800876063d416fe3dce343469ddcfa3e747ae2
brunocu <brunocu@msn.com>
2022-05-20 13:14:44 -0500

    optimization test functions

--- main/test_func.cpp
+++ main/test_func.cpp
@@ -42,3 +42,138 @@ void griewank(const alglib::real_1d_array &x, double &func, void *ptr)
 
 	func = sum - prod + 1;
 }
+
+void rastrigin(const alglib::real_1d_array &x, double &func, void *ptr)
+{
+	const int d = x.length();
+	double sum = 0;
+	for (auto i = 0; i < d; i++)
+	{
+		const auto xi = x[i];
+		// sum = sum + (xi^2 - 10*cos(2*pi*xi))
+		sum = sum + (pow(xi, 2) - 10 * cos(2 * M_PI * xi));
+	}
+
+	func = 10 * d + sum;
+}
+
+void rothyp(const alglib::real_1d_array &x, double &func, void *ptr)
+{
+	const int d = x.length();
+	double outer = 0;
+	for (auto i = 0; i < d; i++)
+	{
+		double inner = 0;
+		for (auto j = 0; j < i; j++)
+		{
+			const auto xj = x[j];
+			inner = inner + pow(xj, 2);
+		}
+		outer = outer + inner;
+	}
+
+	func = outer;
+}
+
+void spheref(const alglib::real_1d_array &x, double &func, void *ptr)
+{
+	const int d = x.length();
+	double sum = 0;
+	for (auto i = 0; i < d; i++)
+	{
+		const auto xi = x[i];
+		sum = sum + pow(xi, 2);
+	}
+
+	func = sum;
+}
+
+void dixonpr(const alglib::real_1d_array &x, double &func, void *ptr)
+{
+	const int d = x.length();
+	const auto x1 = x[0];
+	const auto term1 = pow((x1 - 1), 2);
+	double sum = 0;
+	for (auto i = 1; i < d; i++)
+	{
+		const auto xi = x[i];
+		const auto xold = x[i - 1];
+		// 	new = ii * (2*xi^2 - xold)^2;
+		const auto snew = i * pow((2 * pow(xi, 2) - xold), 2);
+		sum = sum + snew;
+	}
+
+	func = term1 + sum;
+}
+
+void sumsq(const alglib::real_1d_array &x, double &func, void *ptr)
+{
+	const int d = x.length();
+	double sum = 0;
+	for (auto i = 0; i < d; i++)
+	{
+		const auto xi = x[i];
+		sum = sum + i * pow(xi, 2);
+	}
+
+	func = sum;
+}
+
+void trid(const alglib::real_1d_array &x, double &func, void *ptr)
+{
+	const int d = x.length();
+	double sum1 = pow((x[0] - 1), 2);
+	double sum2 = 0;
+	for (auto i = 1; i < d; i++)
+	{
+		const auto xi = x[i];
+		const auto xold = x[i - 1];
+		sum1 = sum1 + pow((xi - 1), 2);
+		sum2 = sum2 + xi * xold;
+	}
+
+	func = sum1 - sum2;
+}
+
+void rosenbrock(const alglib::real_1d_array &x, double &func, void *ptr)
+{
+	const int d = x.length();
+	double sum = 0;
+	for (int i = 0; i < (d - 1); i++)
+	{
+		const auto xi = x[i];
+		const auto xnext = x[i + 1];
+		// new = 100*(xnext-xi^2)^2 + (xi-1)^2;
+		const auto term = 100 * pow((xnext - pow(xi, 2)), 2) + pow((xi - 1), 2);
+		sum = sum + term;
+	}
+
+	func = sum;
+}
+
+void levy(const alglib::real_1d_array &x, double &func, void *ptr)
+{
+	const int d = x.length();
+	alglib::real_1d_array w;
+	w.setlength(d);
+
+	for (auto i = 0; i < d; i++)
+	{
+		w[i] = 1 + (x[i] - 1) / 4;
+	}
+	
+	// term1 = (sin(pi*w(1)))^2;
+	const double term1 = pow(sin(M_PI * w[0]), 2);
+	// term3 = (w(d)-1)^2 * (1+(sin(2*pi*w(d)))^2);
+	const double term3 = pow((w[d - 1] - 1), 2) * (1 + pow(sin(2 * M_PI * w[d - 1]), 2));
+	double sum = 0;
+	for (auto i = 0; i < (d - 1); i++)
+	{
+		const auto wi = w[i];
+		//  new = (wi-1)^2 * (1+10*(sin(pi*wi+1))^2);
+		const auto term = pow((w[i] - 1), 2) * (1 + 10 * pow(sin(M_PI * w[i] + 1), 2));
+		sum = sum + term;
+	}
+
+	func = term1 + sum + term3;
+}
--- main/test_func.h
+++ main/test_func.h
@@ -17,4 +17,28 @@ void ackley(const alglib::real_1d_array &x, double &func, void *ptr);
 // Griewank Function
 void griewank(const alglib::real_1d_array &x, double &func, void *ptr);
 
+// Rastrigin Function
+void rastrigin(const alglib::real_1d_array &x, double &func, void *ptr);
+
+// Rotated Hyper-Ellipsoid Function
+void rothyp(const alglib::real_1d_array &x, double &func, void *ptr);
+
+// Sphere Function
+void spheref(const alglib::real_1d_array &x, double &func, void *ptr);
+
+// Dixon-Price Function
+void dixonpr(const alglib::real_1d_array &x, double &func, void *ptr);
+
+// Sum Squares Function
+void sumsq(const alglib::real_1d_array &x, double &func, void *ptr);
+
+// Trid Function
+void trid(const alglib::real_1d_array &x, double &func, void *ptr);
+
+// Rosenbrock Function
+void rosenbrock(const alglib::real_1d_array &x, double &func, void *ptr);
+
+// Levy Function
+void levy(const alglib::real_1d_array &x, double &func, void *ptr);
+
 #endif // !TEST_FUNC_H
