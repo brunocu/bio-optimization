c2527cda99d95b258f180f4bf58802a56c768759
brunocu <brunocu@msn.com>
2022-05-21 00:39:52 -0500

    Test function evaluations to csv

--- main/main.cpp
+++ main/main.cpp
@@ -7,31 +7,57 @@
 #include <iomanip>
 #include <limits>
 
+struct algo {
+	std::string name;
+	testFunc func;
+	double lower;
+	double upper;
+};
+
 int main()
 {
-	alglib::real_1d_array sol;
-	double func;
-	std::ofstream fout("rainfall_griewank.csv");
+	algo test[]{
+		{"Ackley", ackley, -32.768, 32.768},
+		{"Griewank", griewank, -600, 600},
+		{"Rastrigin", rastrigin, -5.12, 5.12},
+		{"Rotated Hyper-Ellipsoid", rothyp, -65.536, 65.536},
+		{"Sphere", spheref, -5.12, 5.12},
+		{"Dixon-Price", dixonpr, -10, 10},
+		{"Sum Squares", sumsq, -10, 10},
+		{"Trid", trid, -200, 200},
+		{"Rosenbrock", rosenbrock, -5, 10},
+		{"Levy", levy, -10, 10},
+	};
+
+	// write to csv
+	std::ofstream fout("rainfall.csv");
 	// io manipulators
 	fout << std::scientific;
 	fout.precision(std::numeric_limits<double>::digits10);
 	// write column names
+	std::cout << "seed,func,d,min\n";
 	fout << "seed,func,d,min\n";
-	printf("Grienwank\n");
 
+	alglib::real_1d_array sol;
+	double ret;
 	int d[] = { 10, 20 };
 	unsigned seed = 42;
 	const auto clk_ref = clock();
 	for (auto i = 0; i < 30; i++)
 	{
-		for (auto &&n : d)
+		for (auto &funcStruct : test)
 		{
-			std::srand(seed);
-			rainfalloptimize(n, 30, 10, -600.0, 600.0, griewank, sol);
-			griewank(sol, func, NULL);
-			printf("seed=%u\tn=%d d=%d min=%.4f\n", seed, i, n, func);
-			// write to csv
-			fout << seed << ",grienwank," << n << "," << func << "\n";
+			for (auto &n : d)
+			{
+				//printf("%s [%.3f, %.3f] d=%d\n", funcStruct.name.c_str(), funcStruct.lower, funcStruct.upper, n);
+
+				std::srand(seed);
+				rainfalloptimize(n, 30, 10, funcStruct.lower, funcStruct.upper, funcStruct.func, sol);
+				funcStruct.func(sol, ret, NULL);
+				//printf("min=%.4f\nx=%s\n\n", ret, sol.tostring(3).c_str());
+				std::cout << seed << "," << funcStruct.name << "," << n << "," << ret << "\n";
+				fout << seed << "," << funcStruct.name << "," << n << "," << ret << "\n";
+			}
 		}
 		seed = seed + (clock() - clk_ref);
 	}
